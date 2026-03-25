4c956247078f92eff44f466f8d24995e55bf4817
dejanstanisicn-arch <dejan.stanisic.n@gmail.com>
2026-03-25 16:55:35 +0100

    Add multiple new routes for different response types

--- app.py
+++ app.py
@@ -9,5 +9,47 @@ app = Flask(__name__)
 def index():
    return "Zdravo programeri"
 
+@app.route("/primer-string")
+def string():
+   return "Neki ne preterano dugacak tekst"
+
+
+@app.route("/primer-broj")
+def broj():
+   return 265
+
+
+@app.route("/primer-niz")
+def niz():
+   nekiNiz = [1,2,3,4,5]
+   return nekiNiz
+
+
+@app.route("/primer-json")
+def primerJson():
+   data = {
+       "message": "This is a JSON response",
+       "status": "success"
+   }
+   return (data)
+
+
+@app.route("/primer-html")
+def primerHTML():
+   data = """<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <link rel="stylesheet" href="static/style.css">
+</head>
+<body>
+   <h1>Zdravo programeri</h1>
+</body>
+</html>"""
+   return data
+
 if __name__ == '__main__':
    app.run() 
